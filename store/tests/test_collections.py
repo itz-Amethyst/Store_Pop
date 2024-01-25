8145a0bf5b5fc7b958426ee4379b69462034e2a5
itz-amethyst <pransermi@gmail.com>
2024-01-25 18:57:27 +0330

    feat: added some tests

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -2,8 +2,12 @@ from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
 
+from core.models import User
+
+
 @pytest.mark.django_db
 class TestCreateCollection:
+    @pytest.mark.skip # Will skip the test
     def test_if_user_is_anonymous_returns_401(self):
         # Arrange
 
@@ -13,4 +17,30 @@ class TestCreateCollection:
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403( self ):
+
+        client = APIClient()
+        client.force_authenticate(user = {})
+        response = client.post('/store/collections/' , {'title': 'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_return_400( self ):
+
+        client = APIClient()
+        client.force_authenticate(user = User(is_staff = True))
+        response = client.post('/store/collections/' , {'title': ''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_return_201( self ):
+        client = APIClient()
+        client.force_authenticate(user = User(is_staff = True))
+        response = client.post('/store/collections/' , {'title': 'a'})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
+# ptw python test watch command
 
