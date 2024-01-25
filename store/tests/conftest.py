00b737647c739888ae87eb71d1bfaf28a961e6ee
itz-amethyst <pransermi@gmail.com>
2024-01-25 19:44:37 +0330

    feat: added fixtures for test

---
+++ store/tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+
+
+@pytest.fixture
+def api_client():
+    return APIClient
+
+@pytest.fixture
+def authenticate(api_client):
+    def do_authenticate():
+        return api_client.force_authenticate(user = User(is_staff =  True))
+    return do_authenticate
