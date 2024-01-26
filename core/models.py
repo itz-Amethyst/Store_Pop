3ef552d71366a2af00a9c0e59075a2a13dd04a16
itz-amethyst <pransermi@gmail.com>
2024-01-26 20:14:39 +0330

    fix: fixed generate admin unique name

--- core/models.py
+++ core/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from random import randint
 
 # Create your models here.
 class User(AbstractUser):
   email = models.EmailField(unique=True)
+
+  def save(self, *args, **kwargs):
+    if not self.first_name and self.is_staff:
+      unique_number = randint(1000, 9999)
+      self.first_name = f"admin{unique_number}"
+
+    super().save(*args, **kwargs)
