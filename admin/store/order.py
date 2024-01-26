8d18a0a152ee496bd2945ebd700457c7874b254e
itz-amethyst <pransermi@gmail.com>
2024-01-26 20:18:36 +0330

    fix: fixed the method not allowed in admin

--- admin/store/order.py
+++ admin/store/order.py
@@ -8,3 +8,6 @@ class OrderAdmin(admin.ModelAdmin):
     autocomplete_fields = ['customer']
     inlines = [OrderItemInline]
     list_display = ['id', 'placed_at', 'customer']
+    search_fields = ['customer__id']
+    #! Fix the DisallowedModelAdminLookup Filtering by customer__id not allowed
+    list_filter = ['customer']
