8a51ca8b31dab316fc55dea09b9380e02df985b2
itz-amethyst <pransermi@gmail.com>
2024-01-28 22:29:54 +0330

    feat: transactions

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,16 @@
+from django.contrib.contenttypes.models import ContentType
 from django.core.mail import send_mail , mail_admins , BadHeaderError, EmailMessage
 from django.core.serializers import serialize
+from django.db import transaction , connection
+from django.db.models.functions import Concat
+from django.forms import DecimalField
 from django.http import JsonResponse
 from django.shortcuts import render
 from templated_mail.mail import BaseEmailMessage
-from django.db.models import Q, F
+from django.db.models import Q , F , Count , Min , Value , Func , ExpressionWrapper
 
 from store.models import Product , OrderItem , Order
+from tags.models import TaggedItem
 from .tasks.main import notify_customers
 
 
@@ -114,3 +119,61 @@ def test_data_optimizing(request):
 
     return JsonResponse(data = data , safe = False)
 
+def test_data_aggregate(request):
+    object = Product.objects.aggregate(Count(count = 'id', min_price= Min('unit_price')))
+
+    data = serialize('json' , object)
+
+    return JsonResponse(data = data , safe = False)
+
+
+def test_data_annotate(request):
+    # To Avoid Error
+    logic = ExpressionWrapper(F("unit_price" ** 2), output_field = DecimalField())
+
+    object = Product.objects.annotate(is_new = Value("True"))
+
+    object_2 = Product.objects.annotate(new_id = F('id') + 1)
+
+    object_3 = Product.objects.annotate(full_name = Func(F('first_name'), Value(' ') , F('last_name'), function = 'CONCAT'))
+
+    # Same as top but shorter
+    object_4 = Product.objects.annotate(full_name = Concat('first_name', Value(' '), 'last_name'))
+
+    object_5 = Product.objects.annotate(multipled_by_2 = logic)
+
+    data = serialize('json' , object)
+
+    return JsonResponse(data = data , safe = False)
+
+def test_data_generics(request):
+    # 12 => the id of table in django_content_type table
+    content_type = ContentType.objects.get_for_model(Product)
+
+    object = TaggedItem.objects.select_related("tag").filter(content_type = content_type, object_id = 1)
+
+    # Shorter way , by implementing method in manager of the model
+    TaggedItem.objects.get_tags_for(Product, 1)
+
+@transaction.atomic
+def test_save_connected_data(request):
+
+    # .... other codes
+
+    # Here we have a reliable query if the first goes off we might have a orderItem without order but with adding transaction we make it to work either together or none
+    order = Order.objects.create(customer_id = 1)
+
+    OrderItem.objects.create(order = order)
+
+    #? Only applies on this part of code
+    # with transaction.atomic():
+    #     order = Order.objects.create(customer_id = 1)
+    #     OrderItem.objects.create(order = order)
+
+def test_data_raw_sql(request):
+
+    Product.objects.raw("SELECT * FROM store_product")
+
+    with connection.cursor() as cursor:
+        cursor.execute('')
+        cursor.callproc('get_customers', [1,2,3])
